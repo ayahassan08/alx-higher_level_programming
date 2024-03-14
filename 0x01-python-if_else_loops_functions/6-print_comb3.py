2f93c0c4a59199e245be0552d5e4d6b3f4fdff6d
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-14 03:56:29 +0200

    HL1 Commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(10):
-    for number in range(numbers + 1, 10):
-        print(f"{numbers}{number}", end=", " if numbers < 8 else "\n")
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        print(f"{digit1}{digit2}", end=", " if digit1 < 8 else "\n")
