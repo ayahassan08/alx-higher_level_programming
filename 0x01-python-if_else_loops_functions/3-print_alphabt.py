47d5aaca284f1fc8406a42960b9c73cb10c51de4
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-14 03:33:22 +0200

    HL1 Commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    excep = chr(letter)
-    if excep not in "qe":
-        print(excep, end="")
+for letters in range(ord('a'), ord('z') + 1):
+    letter = chr(letters)
+    if letter not in "qe":
+        print(f"{letter}", end="")
