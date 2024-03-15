63f71aceb5464f6baa0a8e4e9e617ca67226af1c
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-15 03:33:05 +0200

    HL2 Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    
-    result = len(sys.argv) - 1
-    if result == 0:
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif result == 1:
-        print ("1 argument:")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(result))
-    for number in range(result):
-        print("{}: {}".format(number + 1, sys.argv[number + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
