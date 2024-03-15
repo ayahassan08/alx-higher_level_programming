33c1dbc13cdec34be15a82436a2e91310117826f
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-15 03:26:02 +0200

    HL2 Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = len(sys.argv) - 1
+    if result == 0:
+        print("0 arguments.")
+    elif result == 1:
+        print ("1 argument:")
+    else:
+        print("{} arguments: ".format(result))
+    for number in range(result):
+        print("{}: {}".format(number + 1, sys.argv[number + 1]))
