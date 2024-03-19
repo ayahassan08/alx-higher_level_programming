7df12c8d084e44aa2f69f1bebe3f28cc82092af4
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 03:49:34 +0200

    HL03 Commit

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    newList = []
+    if my_list:
+        for number in my_list:
+            newList.append(False if number & 2 else True)
+        return newList
