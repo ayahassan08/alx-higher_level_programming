c7b635d0237edef04834ff81c3b0dc0c5af20554
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 04:17:07 +0200

    HL03 Commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * aux_pal - checks if a list is a palindrome
+ * @head: head of the list
+ * @end: end of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int aux_pal(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+	{
+		return (1);
+	}
+
+	if (aux_pal(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: a head of a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+	{
+		return (1);
+	}
+
+	return (aux_pal(head, *head));
+}
