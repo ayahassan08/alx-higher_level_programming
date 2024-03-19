7d3e696b2de362cb67fafe7034a0e4bddeb2745c
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 03:53:04 +0200

    HL03 Commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    if my_list is None:
-        return None
-    newList = []
-    for number in my_list:
-        if (number % 2) == 0:
-            newList.append(True)
-        else:
-            newList.append(False)
-        return newList
+    new_list = []
+    if my_list:
+        for num in my_list:
+            new_list.append(False if num % 2 else True)
+        return new_list
