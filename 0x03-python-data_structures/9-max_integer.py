f7d878b38824a9b8806d39a8f12024d6a5ec247e
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 03:40:12 +0200

    HL03 Commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    else:
+    if my_list:
         my_list.sort(reverse=True)
         return(my_list[0])
+    return None
