10a7cc0e038abf626b5da11c68bf6ee548d7d1a8
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 03:29:17 +0200

    HL03 Commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     if not sentence:
         sentence = None
     if sentence:
-        sen_len = len(sentence)
+        sLen = len(sentence)
     else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
+        sLen = 0
+    return (sLen, sentence if not sentence else sentence[:1])
