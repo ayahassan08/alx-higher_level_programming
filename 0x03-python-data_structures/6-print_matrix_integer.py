e3f1f2b44dad02b42e83e4f56fa3f7741dde04ec
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-19 02:57:04 +0200

    HL03 Commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,6 +7,5 @@ def print_matrix_integer(matrix=[[]]):
         if len(mat) == 0:
             print ()
         for number in range(len(mat)):
-            print("{}".format(mat[number]), end="\n"
-                  if number is len(mat) - 1
-                  else " ")
+            print("{}".format(mat[number]), 
+                  end="\n" if number is len(mat) - 1 else " ")
