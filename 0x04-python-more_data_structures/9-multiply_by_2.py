7bc2ba508c04b92e86b2b7e82545a02dc5f63f82
Aya Hassan <ayahassan.ah74@gmail.com>
2024-03-21 01:10:30 +0200

    HL04 Commit

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    newDiction = {}
+    for key, value in a_dictionary.items():
+        newDiction[key] = value * 2
+        return newDiction
