e6ce6aa88b48bdddb8d19cd568b976d240fe8352
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-15 14:08:54 +0200

    HLA00

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Module for inherits_from method.'''
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    '''Determines if an object is a true subclass of a class.'''
-    return isinstance(obj, a_class) and type(obj) != a_class
+	"""Checks if an object is an inherited instance of a class."""
+	return isinstance(obj, a_class) and type(obj) != a_class
