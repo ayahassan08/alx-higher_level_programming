2e5f715cd0ec61fe16500ad68195e09b14967ae9
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-17 01:50:17 +0200

    HLB00

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,6 +6,7 @@ class Student:
     '''Represents a student'''
     
     def __init__(self, first_name, last_name, age):
+        '''Defines a student'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age        
