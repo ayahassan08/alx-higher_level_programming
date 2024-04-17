c7ed8cf7af771e5035bb204e1e8f52e702e7ff91
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-17 02:03:23 +0200

    HLB00

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,5 +14,5 @@ class Student:
     def to_json(self, attrs=None):
         '''Get a dict. representation of the Student.'''
         if (type(attrs) == list and all(type(element) == str for element in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+            return ({k: getattr(self, k) for k in attrs if hasattr(self, k)})
+        return (self.__dict__)
