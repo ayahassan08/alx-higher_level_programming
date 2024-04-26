65414b2be8cc623f481590e94b98fc83d4f70f37
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 21:15:36 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,8 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''Returns the dictionary representation of a Rectangle'''
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
