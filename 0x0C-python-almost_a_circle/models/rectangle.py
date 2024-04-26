0dc23a491da0de903a31221273d56e48ed9c354a
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 20:25:12 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,48 +4,56 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	'''A Rectangle class'''
+    '''A Rectangle class'''
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		'''A class constructor'''
-		super().__init__(id)
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''A class constructor'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	def width(self):
-		'''Width of rectangle'''
-		return self.__width
+    @property
+    def width(self):
+        '''Width of rectangle'''
+        return self.__width
 
-	def width(self, value):
-		'''Width setter'''
-		self.validate_integer("width", value, False)
-		self.__width = value
+    @width.setter
+    def width(self, value):
+        '''Width setter'''
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-	def height(self):
-		'''Height of rectangle'''
-		return self.__height
+    @property
+    def height(self):
+        '''Height of rectangle'''
+        return self.__height
 
-	def height(self, value):
-		'''Height setter'''
-		self.validate_integer("height", value, False)
-		self.__height = value
+    @height.setter
+    def height(self, value):
+        '''Height setter'''
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-	def x(self):
-		'''x of rectangle'''
-		return self.__x
+    @property
+    def x(self):
+        '''x of rectangle'''
+        return self.__x
 
-	def x(self, value):
-		'''x setter'''
-		self.validate_integer("x", value)
-		self.__x = value
+    @x.setter
+    def x(self, value):
+        '''x setter'''
+        self.validate_integer("x", value)
+        self.__x = value
 
-	def y(self):
-		'''y of rectangle'''
-		return self.__y
+    @property
+    def y(self):
+        '''y of rectangle'''
+        return self.__y
 
-	def y(self, value):
-		'''y setter'''
-		self.validate_integer("y", value)
-		self.__y = value
+    @y.setter
+    def y(self, value):
+        '''y setter'''
+        self.validate_integer("y", value)
+        self.__y = value
