eac2245af0d19a99df59251cf85b96b843dd4547
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 21:11:46 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,14 @@ class Square(Rectangle):
         '''Returns string info'''
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        '''Size of square.'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''Size setter'''
+        self.width = value
+        self.height = value
