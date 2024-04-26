fe1f287e9ad851ba2131e1181f706ac428ef34c2
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 20:30:37 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -57,3 +57,12 @@ class Rectangle(Base):
         '''y setter'''
         self.validate_integer("y", value)
         self.__y = value
+        
+    def validate_integer(self, name, value, eq=True):
+        '''Adding validation of all setter methods and instantiation'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
