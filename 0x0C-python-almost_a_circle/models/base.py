2be85b03904d70e73c8ec8799431194fafe96cd4
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 21:24:56 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,6 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
     def to_json_string(list_dictionaries):
         '''Returns the JSON string representation of list_dictionaries'''
         if list_dictionaries is None or not list_dictionaries:
@@ -24,7 +23,6 @@ class Base:
         else:
             return dumps(list_dictionaries)
 
-    @classmethod
     def save_to_file(cls, list_objs):
         '''Writes the JSON string representation of list_objs to a file'''
         if list_objs is not None:
@@ -32,7 +30,6 @@ class Base:
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
 
-    @staticmethod
     def from_json_string(json_string):
         '''Returns the list of the JSON string representation json_string'''
         if json_string is None or not json_string:
