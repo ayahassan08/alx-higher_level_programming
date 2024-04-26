64de1760e9eac8531c1d3b9d7286e17a65e9e55d
Aya Hassan <ayahassan.ah74@gmail.com>
2024-04-26 20:46:35 +0300

    HLC00

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,69 +4,69 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	'''A Rectangle class'''
+    '''A Rectangle class'''
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		'''A class constructor'''
-		super().__init__(id)
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''A class constructor'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	@property
-	def width(self):
-		'''Width of rectangle'''
-		return self.__width
+    @property
+    def width(self):
+        '''Width of rectangle'''
+        return self.__width
 
-	@width.setter
-	def width(self, value):
-		'''Width setter'''
-		self.validate_integer("width", value, False)
-		self.__width = value
+    @width.setter
+    def width(self, value):
+        '''Width setter'''
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-	@property
-	def height(self):
-		'''Height of rectangle'''
-		return self.__height
+    @property
+    def height(self):
+        '''Height of rectangle'''
+        return self.__height
 
-	@height.setter
-	def height(self, value):
-		'''Height setter'''
-		self.validate_integer("height", value, False)
-		self.__height = value
+    @height.setter
+    def height(self, value):
+        '''Height setter'''
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-	@property
-	def x(self):
-		'''x of rectangle'''
-		return self.__x
+    @property
+    def x(self):
+        '''x of rectangle'''
+        return self.__x
 
-	@x.setter
-	def x(self, value):
-		'''x setter'''
-		self.validate_integer("x", value)
-		self.__x = value
+    @x.setter
+    def x(self, value):
+        '''x setter'''
+        self.validate_integer("x", value)
+        self.__x = value
 
-	@property
-	def y(self):
-		'''y of rectangle'''
-		return self.__y
+    @property
+    def y(self):
+        '''y of rectangle'''
+        return self.__y
 
-	@y.setter
-	def y(self, value):
-		'''y setter'''
-		self.validate_integer("y", value)
-		self.__y = value
+    @y.setter
+    def y(self, value):
+        '''y setter'''
+        self.validate_integer("y", value)
+        self.__y = value
 
-	def validate_integer(self, name, value, eq=True):
-		'''Adds validation of setter method and instantiation'''
-		if type(value) != int:
-			raise TypeError("{} must be an integer".format(name))
-		if eq and value < 0:
-			raise ValueError("{} must be >= 0".format(name))
-		elif not eq and value <= 0:
-			raise ValueError("{} must be > 0".format(name))
+    def validate_integer(self, name, value, eq=True):
+        '''Adds validation of setter method and instantiation'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
 
-	def area(self):
-		'''Returns the area value of the Rectangle'''
-		return self.width * self.height
+    def area(self):
+        '''Returns the area value of the Rectangle'''
+        return self.width * self.height
